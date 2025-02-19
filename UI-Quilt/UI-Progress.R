14c3147f4572974ceba7a63395c3057e5ca2ac82
hannahcrook <hannahcrook@vt.edu>
2025-02-19 12:04:44 -0500

    first tab progress!

--- UI-Quilt/UI-Progress.R
+++ UI-Quilt/UI-Progress.R
@@ -6,39 +6,63 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Design Your Quilt!"),
+    titlePanel("Climate Quilt!"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    tabsetPanel(
+      tabPanel("Design", 
+               h3("Design Your Quilt!"),
+               p(  
+                 # Sidebar with a drop-down input for size of quilt
+                 sidebarLayout(
+                   sidebarPanel(
+                     selectInput("quiltsize",
+                                "Choose Quilt Size",
+                                 choices = c("5x7 (Baby)",
+                                             "6x9 (Crib)",
+                                             "9x11 (Throw)",
+                                             "12x15 (Twin)",
+                                             "14x18 (Full)",
+                                             "15x18 (Queen)",
+                                             "18x18 (King)")),
+                     helpText("Note: all squares will be 6 inches by 6 inches"),
+                  
+                     # Also in sidebar with drop-down for number of colors   
+                     selectInput("colorquantity",
+                                 "Choose Amount of Colors",
+                                 choices = c("5",
+                                             "6",
+                                             "7",
+                                             "8",
+                                             "9",
+                                             "10",
+                                             "11",
+                                             "12"))
+                     ),
+                   
+                   # Show Images of color patches, making them selectable buttons
+                   mainPanel(
+                     plotOutput("distPlot")
+                   )
+                 ))),
+      
+      tabPanel("Data Setup", 
+               h3("Choose Your Data!"),
+               p("example"
+                 
+               )),
+      
+      tabPanel("View & Share",
+               h3("Preview Your Design & Share!"),
+               p("This is example content for the third tab.")),
+      
+      tabPanel("User Guide",
+               h3("How to Use App!"),
+               p("This is example content for the third tab."))
+    ),
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
+server <- function(input, output) {}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
