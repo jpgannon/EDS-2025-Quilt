a0e4e0d53069a7da0bc420ce222c282284028479
hannahcrook <hannahcrook@vt.edu>
2025-02-24 22:04:22 -0500

    second tab UI

--- UI-Quilt/UI-Progress.R
+++ UI-Quilt/UI-Progress.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
 
     # Application title
@@ -41,23 +41,60 @@ ui <- fluidPage(
                    
                    # Show Images of color patches, making them selectable buttons
                    mainPanel(
-                     plotOutput("distPlot")
+                     plotOutput("distPlo")
                    )
                  ))),
       
       tabPanel("Data Setup", 
                h3("Choose Your Data!"),
-               p("example"
+               p(
+                 #Default Dataset Selection
+                 mainPanel(
+                   selectInput("defaultdataselect",
+                               "Choose Your Data Type!", 
+                               choices = c("Temperature",
+                                           "Water Chemistry",
+                                           "Soils",
+                                           "Vegetation",
+                                           "Heterotrophs")),
+                   
+                   helpText("OR"),
+                   
+                   #Data Upload button
+                   fileInput("fileupload", "Upload Your Own Data File!",
+                           accept = ".csv"),
+                   textOutput("dataInfo"),
+                 
+                   #Select Time Period of Data
+                   helpText("Now select the time period your quilt will show!"),
+                   dateInput("dataStartDate", "Select a Start Date:", 
+                             value = Sys.Date(),
+                             format = "mm/dd/yyyy"),
+                   dateInput("dataEndDate", "Select an End Date:", 
+                             value = Sys.Date(),
+                             format = "mm/dd/yyyy"),
+               )
                  
                )),
       
       tabPanel("View & Share",
                h3("Preview Your Design & Share!"),
-               p("This is example content for the third tab.")),
+               p(
+                 sidebarLayout(
+                   sidebarPanel(
+                     
+                     downloadButton("downloadQuilt,
+                                    Download Quilt Pattern")
+                     
+                   )
+                 )
+               )),
       
       tabPanel("User Guide",
                h3("How to Use App!"),
-               p("This is example content for the third tab."))
+               p(
+                 helpText("Tutorial on How to Use our App!")
+               ))
     ),
 )
 
